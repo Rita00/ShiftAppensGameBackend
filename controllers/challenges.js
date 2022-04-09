5fb559d1afa468986a8725358d4447edc9742a84
FMiranda97 <brugesmiranda@gmail.com>
2022-04-09 20:14:14 +0100

    fix code validation

--- controllers/challenges.js
+++ controllers/challenges.js
@@ -44,38 +44,49 @@ exports.validateCode = (req, res, next) => {
     const idChallenge = req.body.idChallenge;
     const code = req.body.code;
     Challenge.findOne({ _id: idChallenge, availableCodes: { "$in": [code] } })
-        .then((challengeDocument) => {
-            if (!challengeDocument) {
-                const error = new Error("Código inválido");
-                throw error;
-            }
-            User.findOne({ _id: req.userId, completedChallenges: { "$nin": [challengeDocument] } })
-                .then(userDocument => {
-                    if (!userDocument) {
-                        const error = new Error("Ação inválida");
-                        throw error;
-                    }
-                    userDocument.completedChallenges.push(challengeDocument);
-                    userDocument.totalPoints += challengeDocument.points;
-                    challengeDocument.availableCodes.pull(code)
-                    challengeDocument.save();
-                    userDocument.save();
-                    res.status(201).json({
-                        msg: "Desafio concluído"
-                    })
-                }).catch(error => {
-                    res.status(403).json({
-                        msg: "Ação inválida"
-                    })
-                });
-
-            console.log(challengeDocument);
-        })
-        .catch(error => {
+    .select('availableCodes').exec((error, challengeDocument) => {
+        console.log(challengeDocument);
+        if (error) {
             res.status(400).json({
                 msg: ""
             })
-        })
+            return;
+        }
+
+        if (!challengeDocument) {
+            const error = new Error("Código inválido");
+            throw error;
+        }
+
+        User.findOne({ _id: req.userId, completedChallenges: { "$nin": [challengeDocument] } })
+            .then(userDocument => {
+                console.log('aqui');
+                console.log(userDocument);
+                console.log(challengeDocument);
+                if (!userDocument) {
+                    const error = new Error("Ação inválida");
+                    throw error;
+                }
+                userDocument.completedChallenges.push(challengeDocument);
+                console.log('aqui2');
+                userDocument.totalPoints += challengeDocument.points;
+                console.log('aqui3');
+                challengeDocument.availableCodes.pull(code)
+                console.log('aqui4');
+                challengeDocument.save();
+                console.log('aqui5');
+                userDocument.save();
+                console.log('aqui6');
+                res.status(201).json({
+                    msg: "Desafio concluído"
+                })
+            }).catch(error => {
+                res.status(403).json({
+                    msg: "Ação inválida"
+                })
+            });
+
+    })
 };
 
 exports.generateCodes = (req, res, next) => {
